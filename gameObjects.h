b4254be2f27b4eb619b345848252d32a96defcb2
Chris0lsen <chris@chrisolsen.net>
2014-08-28 13:46:21 -0500

    Placed class declarations in separate headers

--- gameObjects.h
+++ gameObjects.h
@@ -1,4 +1,42 @@
 #ifndef GAMEOBJECTS_H
 #define GAMEOBJECTS_H
 
+namespace olsen{
+	
+	/*----------------------------------------------------------------------
+     Requirement #16A: Demonstrate class
+     ----------------------------------------------------------------------*/
+    // Base class for difficulties
+	class Game {
+	public:
+		
+	private:
+		
+		
+	}
+	
+	/*----------------------------------------------------------------------
+     Requirement #24A: Demonstrate inheritance
+     ----------------------------------------------------------------------*/
+    class BeginnerGame : public Game {
+	/*----------------------------------------------------------------------
+     Requirement #24B: Demonstrate polymorphism
+     ----------------------------------------------------------------------*/
+		
+	};
+
+	class EasyGame : public Game {
+		
+	};
+	
+	class MediumGame : public Game {
+		
+		
+	};
+	
+	class HardGame : public Game {
+		
+	};
+}
+
 #endif
--- menu.h
+++ menu.h
@@ -1,4 +1,53 @@
 #ifndef MENU_H
 #define MENU_H
+#include <iostream>
+#include <string>
+using std::cin;
+using std::string;
+
+	/*----------------------------------------------------------------------
+     Requirement #18: Demonstrate namespace
+     ----------------------------------------------------------------------*/
+namespace olsen{
+	class Menu {
+	public:
+	
+		bool isValid();
+	/*----------------------------------------------------------------------
+     Requirement #08B: Demonstrate an overloaded function
+     ----------------------------------------------------------------------*/
+	 bool isValid(int x);
+	 bool isValid(const char*);
+	 bool isValid(string str);
+		int GetSelection();
+	private:
+		
+		
+	}
+	
+	
+	
+	int Menu::GetSelection(){
+		
+	}
+	
+    /*----------------------------------------------------------------------
+     Requirement #07: Demonstrate a debugging trick
+     ----------------------------------------------------------------------*/	
+	bool Menu::isValid(){
+	// Validate input for proper data type
+		if (!cin){
+			return false;
+		}
+	}
+	
+	
+}
+	
+	
+	
+	
+	
+	
 
 #endif
