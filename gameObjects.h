d37904e256716ba539534f4de9fbb23f4f9f737f
Chris0lsen <chris@chrisolsen.net>
2014-08-31 15:15:06 -0500

    Closing in

--- gameObjects.h
+++ gameObjects.h
@@ -1,6 +1,23 @@
 #ifndef GAMEOBJECTS_H
 #define GAMEOBJECTS_H
 
+#define BOARD_C 2
+#define BOARD_R 5
+#define ARRAYSIZE 10
+#define BEGIN_LEN 4
+#define EASY_LEN 5
+#define MEDIUM_LEN 6
+#define HARD_LEN 8
+
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
 namespace olsen{
 	
 	/*----------------------------------------------------------------------
@@ -9,33 +26,64 @@ namespace olsen{
     // Base class for difficulties
 	class Game {
 	public:
-		
+		//declare board size 
+		//string? DUMBASS 
+		 Game();
+		~Game();
+		/*----------------------------------------------------------------------
+		Requirement #14A: Demonstrate a multi-dimensional array
+		----------------------------------------------------------------------*/
+		string board[BOARD_R][BOARD_C];
+		char randC[10] = {'<', '&', ']', ';', '%', '@', '+','#', '^','{'};
+		bool turn(string*, string); 
+		string guessWord(string*);
+		void play(string*, string);
+		/*----------------------------------------------------------------------
+		Requirement #16B: Demonstrate struct
+		----------------------------------------------------------------------*/
+		struct iWord{
+			char letter;
+			/*----------------------------------------------------------------------
+			Requirement #17B: Demonstrate pointer to struct
+    `		 ----------------------------------------------------------------------*/
+			iWord* next;
+		};
+    
 	private:
 		
 		
-	}
+	};
 	
 	/*----------------------------------------------------------------------
      Requirement #24A: Demonstrate inheritance
      ----------------------------------------------------------------------*/
     class BeginnerGame : public Game {
+		public:
 	/*----------------------------------------------------------------------
      Requirement #24B: Demonstrate polymorphism
      ----------------------------------------------------------------------*/
-		
+		void play(string*, string);
+		bool turn(string*, string);
+		 BeginnerGame();
+		~BeginnerGame();
 	};
 
 	class EasyGame : public Game {
-		
+		public:
+		 EasyGame();
+		~EasyGame();
 	};
 	
 	class MediumGame : public Game {
-		
-		
+		public:
+		 MediumGame();
+		~MediumGame();
 	};
 	
 	class HardGame : public Game {
-		
+		public:
+		 HardGame();
+		~HardGame();
 	};
 }
 
--- menu.h
+++ menu.h
@@ -4,7 +4,7 @@
 #include <string>
 using std::cin;
 using std::string;
-
+enum difficulty {beginner, easy, medium, hard};
 	/*----------------------------------------------------------------------
      Requirement #18: Demonstrate namespace
      ----------------------------------------------------------------------*/
@@ -16,19 +16,57 @@ namespace olsen{
 	/*----------------------------------------------------------------------
      Requirement #08B: Demonstrate an overloaded function
      ----------------------------------------------------------------------*/
-	 bool isValid(int x);
+	 bool isValid(int x); //we should use this somewhere DUMBASS 
 	 bool isValid(const char*);
 	 bool isValid(string str);
-		int GetSelection();
+	int GetSelection();
 	private:
 		
 		
-	}
+	};
 	
 	
 	
 	int Menu::GetSelection(){
+		//declare variables 
+		char choice;
+	
+		//ask the user what difficulty of game they want to play 
+		cout << "Please select a difficulty for the game: (B)eginner, (E)asy, (N)ormal, (H)ard" << endl;
+	
+		/*----------------------------------------------------------------------
+		Requirement #01B: Demonstrate simple input
+		----------------------------------------------------------------------*/
+		cin >> choice;
 		
+		if (this->isValid()){ //error catching 
+			//Continue
+		} else{
+		/*----------------------------------------------------------------------
+		Requirement #13: Demonstrate recursion
+		----------------------------------------------------------------------*/
+		//Recursively call until input is valid
+			cout << "Please enter a valid choice." << endl << endl; 
+			cin.clear();
+			cin.ignore();
+			this->GetSelection();
+		}
+		
+	//add choice for quitting the game? DUMBASS 
+		switch(choice) {
+			case 'B':
+				return beginner;
+				//add break DUMBASS
+			case 'E':
+				return easy;
+			case 'N':
+				return medium;
+			case 'H':
+				return hard;
+		}
+	
+		//should never get called
+		return medium;
 	}
 	
     /*----------------------------------------------------------------------
@@ -40,14 +78,6 @@ namespace olsen{
 			return false;
 		}
 	}
-	
-	
 }
-	
-	
-	
-	
-	
-	
 
 #endif
